d0a8e624071e6799d6d9faa773e2bd53be9ee35d
Bindhu <biji_100@yahoo.com>
2019-03-30 12:38:52 -0600

    update changes

--- config/orm.js
+++ config/orm.js
@@ -4,10 +4,7 @@
 var connection = require("../config/connection.js");
 
 // Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
+
 function printQuestionMarks(num) {
   var arr = [];
 
@@ -72,7 +69,7 @@ var orm = {
       cb(result);
     });
   },
-  // An example of objColVals would be {name: panther, sleepy: true}
+  // An example of objColVals would be 
   update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
@@ -106,7 +103,7 @@ var orm = {
   // }
 };
 
-// Export the orm object for the model (cat.js).
+// Export the orm object for the model (burger.js).
 module.exports = orm;
 
 
