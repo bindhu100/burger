f97ada1c96d8b315bafdaeb3ad10af7e5a9b19c0
Bindhu <biji_100@yahoo.com>
2019-03-31 17:32:50 -0600

    update changes

--- config/orm.js
+++ config/orm.js
@@ -28,7 +28,7 @@ function objToSql(ob) {
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-     
+
       arr.push(key + "=" + value);
     }
   }
@@ -39,16 +39,16 @@ function objToSql(ob) {
 
 // Object for all our SQL statement functions.
 var orm = {
-  all: function(tableInput, cb) {
+  all: function (tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
+    connection.query(queryString, function (err, result) {
       if (err) {
         throw err;
       }
       cb(result);
     });
   },
-  create: function(table, cols, vals, cb) {
+  create: function (table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
     queryString += " (";
@@ -60,7 +60,7 @@ var orm = {
 
     console.log(queryString);
 
-    connection.query(queryString, vals, function(err, result) {
+    connection.query(queryString, vals, function (err, result) {
       if (err) {
         throw err;
       }
@@ -69,7 +69,7 @@ var orm = {
     });
   },
   // An example of objColVals would be 
-  update: function(table, objColVals, condition, cb) {
+  update: function (table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
     queryString += " SET ";
@@ -78,7 +78,7 @@ var orm = {
     queryString += condition;
 
     console.log(queryString);
-    connection.query(queryString, function(err, result) {
+    connection.query(queryString, function (err, result) {
       if (err) {
         throw err;
       }
@@ -86,7 +86,7 @@ var orm = {
       cb(result);
     });
   },
-  
+
   // delete: function(table, condition, cb) {
   //   var queryString = "DELETE FROM " + table;
   //   queryString += " WHERE ";
--- models/burger.js
+++ models/burger.js
@@ -7,20 +7,20 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
+  all: function (cb) {
+    orm.all("burgers", function (res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
+  create: function (cols, vals, cb) {
+    orm.create("burgers", cols, vals, function (res) {
       cb(res);
     });
   },
-  update: function(id, cb) {
-    var condition = "id ="+id;
-    orm.update("burgers", {devoured:true}, condition, function(res) {
+  update: function (id, cb) {
+    var condition = "id =" + id;
+    orm.update("burgers", { devoured: true }, condition, function (res) {
       cb(res);
     });
   },
